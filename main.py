2e10f6f0570edb79abc664d8b7d37f89f2e19f43
unknown <ayush02av@gmail.com>
2023-01-10 13:56:24 +0530

    Prediction route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
@@ -13,6 +14,66 @@ def dashboard():
 @app.route('/about', methods = ['GET'])
 def about():
     return render_template('about.html')
+
+model_charges = pickle.load(open('model_charges.pkl', 'rb'))
+model_region = pickle.load(open('model_region.pkl', 'rb'))
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = request.get_json()
+    data_keys = data.keys()
+    errors = []
+    
+    validators = [
+        'age',
+        'female',
+        'bmi',
+        'children',
+        'smoker'
+    ]
+    for validator in validators:
+        if validator not in data_keys:
+            errors.append(f"{validator} is required")
+    
+    if len(errors) > 0:
+        return jsonify({
+            'success': 0,
+            'errors': errors
+        })
+    
+    if 'region' not in data_keys:
+        data['region'] = model_region.predict([
+                [
+                    data['age'],
+                    data['female'],
+                    data['bmi'],
+                    data['children'],
+                    data['smoker']
+                ]
+            ]).tolist()[0]
+    
+    prediction = round(model_charges.predict([
+            [
+                data['age'],
+                data['female'],
+                data['bmi'],
+                data['children'],
+                data['smoker'],
+                data['region'],
+            ]
+        ]).tolist()[0], 2)
+
+    if prediction < 100:
+        return jsonify({
+            'success': 0,
+            'errors': ['Our Model does not serve to your request']
+        })
+    
+    return jsonify({
+        'success': 1,
+        'input': data,
+        'output' : prediction
+    })
 
 if __name__ == "__main__":
     app.run(debug = True, port = 80)
